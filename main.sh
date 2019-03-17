8f6ba3468dc42d5751e707a979e446f124b51635
Alexander Morosow <alexander.morosow@googlemail.com>
2019-03-16 18:53:54 -0700

    fix order of calls

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 DATASET_URL=$1
 DATASET_NAME=$2
 
-wget -rnd --no-parent $DATASET_URL -P /storage/datasets/$DATASET_NAME
+wget -rnd --no-parent $DATASET_URL -P /storage/datasets/$DATASET_NAME &&
 
 python train.py --dataset $DATASET_NAME
 
